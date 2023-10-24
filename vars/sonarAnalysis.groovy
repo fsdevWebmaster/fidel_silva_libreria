620200651b3fb7e2cacadeb2d6c732b0f2e5d64c
fsdevWebmaster <119712167+fsdevWebmaster@users.noreply.github.com>
2023-10-24 13:48:26 -0500

    Delete vars/sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++
@@ -1,8 +0,0 @@
-def call(sonarenv = "false") {
-  timeout(time: 5, unit: "MINUTES") {
-    sh 'echo Ejecución de las pruebas de calidad de código'
-    if(sonarenv != "false") {
-      sh "echo Abort process: ${sonarenv}"
-    }
-  }
-}
