d8a4e2eec5461a7fc2ba745e3adf0567559a38bb
fsdevWebmaster <119712167+fsdevWebmaster@users.noreply.github.com>
2023-10-24 22:43:15 -0500

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,5 +1,5 @@
 def call() {
   sh "echo Ejecución de las pruebas de calidad de código."
-  sh "/Users/fidelsilva/Documents/UPC/master/modulo_5/backapp/sonar-scanner"
+  sh "/usr/local/bin/sonar-scanner"
   
 }
