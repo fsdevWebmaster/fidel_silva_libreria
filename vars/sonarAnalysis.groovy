611e1b68ce9eea9cbc71172f8cd5091baf2711a5
fsdevWebmaster <119712167+fsdevWebmaster@users.noreply.github.com>
2023-10-24 19:27:36 -0500

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -2,7 +2,6 @@ def call() {
   sh "echo Ejecución de las pruebas de calidad de código."
   sonar-scanner \
     -Dsonar.projectKey=sonar-first \
-    -Dsonar.sources=.\
     -Dsonar.host.url=http://localhost:9000 \
     -Dsonar.token=sqp_5028a7dc8205bde87db68c6790357489b3a319ff
 }
