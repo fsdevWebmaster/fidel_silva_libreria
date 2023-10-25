45933983403564121cd97d03fcbed505cf9ab762
fsdevWebmaster <119712167+fsdevWebmaster@users.noreply.github.com>
2023-10-25 11:25:05 -0500

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,4 +1,8 @@
-def call() {
+def call(abortPipeline = false) {
   sh "echo Ejecución de las pruebas de calidad de código."
   sh "/usr/local/bin/sonar-scanner"
+  timeout(time: 2, unit: "MINUTES") {
+      // waitForQualityGate abortPipeline: false
+      return abortPipeline
+  }  
 }
