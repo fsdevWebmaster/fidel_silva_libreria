dd384afe46727b84af7d2f617cc952b313eb1cb1
fsdevWebmaster <119712167+fsdevWebmaster@users.noreply.github.com>
2023-10-25 01:32:29 -0500

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,8 +1,4 @@
 def call() {
   sh "echo Ejecución de las pruebas de calidad de código."
   sh "/usr/local/bin/sonar-scanner"
-  withSonarQubeEnv(credentialsId: 'new-creds', installationName: 'sonarqube-server') {
-    def qg = waitForQualityGate()
-    sh "echo QG STATUS:: ${qg.status}"
-  }
 }
